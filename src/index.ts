bdbb4216e91e9accc338017706cbd54bcfea5892
amaymerich <91441575+amaymerich@users.noreply.github.com>
2022-08-26 15:34:01 -0300

    Game 1

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,61 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Loader, Ticker } from 'pixi.js'
+import { assets } from './Assets';
+import { TickerScene } from './scenes.ts/TickerScene';
 
-const app = new Application({
+
+
+//Aplicaion de pxi que renderiza por nosotros
+export const WIDTH=1920;
+export const HEIGHT=1080;
+
+const app = new Application({ 
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
-	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	
+	backgroundColor:0x6495ed,
+	width: WIDTH,
+	height: HEIGHT,
+});
+Keyboard.initialize();
+
+
+//Acomoda la pantalla
+window.addEventListener("resize", ()=>{
+	const scalex = window.innerWidth/app.screen.width;
+	const scaley = window.innerHeight / app.screen.height;
+	const scale= Math.min(scalex,scaley);
+
+	
+	const gamewidth = Math.round(app.screen.width * scale);
+	const gameheight =  Math.round(app.screen.height * scale);
+
+	
+	const marginHorizontal = Math.floor((window.innerWidth - gamewidth)/2);
+	const marginVertical = Math.floor((window.innerHeight - gameheight)/2);
+
+	
+	app.view.style.width = gamewidth+ "px";
+	app.view.style.height = gameheight + "px";
+
+	
+	app.view.style.marginLeft = marginHorizontal +"px";
+	app.view.style.marginRight = marginHorizontal +"px";
+
+	app.view.style.marginTop = marginVertical + "px";
+	app.view.style.marginBottom = marginVertical + "px";
+})
+window.dispatchEvent(new Event("resize"));
+Loader.shared.add(assets);
+Loader.shared.onComplete.add(()=>{
+	const myScene= new TickerScene();
+	app.stage.addChild(myScene);
+	
+	Ticker.shared.add(function(deltaFrame){
+		myScene.update(Ticker.shared.deltaMS,deltaFrame)
+	})
+
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
 
-clampy.anchor.set(0.5);
-
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
-
-app.stage.addChild(clampy);
+Loader.shared.load(); 
